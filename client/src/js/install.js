2a07de138e20f299cc6bd7f88119cfc749cd8404
Tudda <trezzt0224@gmail.com>
2023-07-03 20:28:14 -0500

    updated install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -15,10 +15,23 @@ window.addEventListener('beforeinstallprompt', (event) => {
 butInstall.addEventListener('click', async () => {
 
     const promptEvent = window.deferredPrompt;
-    
 
+    if (!promptEvent) {
+        return;
+    }
+
+    // Show prompt
+    promptEvent.prompt();
+
+    // Reset the deferred prompt variable, it can only be used once.
+    window.deferredPrompt = null;
+
+    butInstall.classList.toggle('hidden', true);
 });
 
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    // Clear prompt
+    window.deferredPrompt = null;
+});
