6e3b44f3c56aa9f6e41173e903d44acd15b91790
Tudda <trezzt0224@gmail.com>
2023-07-03 20:36:26 -0500

    adding comments to sw.js for understanding

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,3 +1,7 @@
+// Caching js and css requires workbox-strategies to be installed
+// To actually respond to requests with a cached response, we need to use a strategy called StaleWhileRevalidate
+// This strategy will first check the cache for a response, and if it finds one, it will return it.
+
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
 const { CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
@@ -5,11 +9,14 @@ const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
+// The precacheAndRoute() method takes an array of URLs to precache. The self._WB_MANIFEST is an array that contains the list of URLs to precache.
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
+  // Name of the cache storage.
   cacheName: 'page-cache',
   plugins: [
+    // This plugin will cache responses with these headers to a maximum-age of 30 days
     new CacheableResponsePlugin({
       statuses: [0, 200],
     }),
